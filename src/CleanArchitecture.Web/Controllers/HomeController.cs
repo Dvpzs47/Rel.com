12401ded14ad1e71813228759a3f4e488fea4007
Dvpzs 47 <Dvpzs47@gmail.com>
2021-03-01 14:39:54 +0100

    home page

--- src/CleanArchitecture.Web/Controllers/HomeController.cs
+++ src/CleanArchitecture.Web/Controllers/HomeController.cs
@@ -4,10 +4,6 @@ namespace Rel.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         public IActionResult Error()
         {
