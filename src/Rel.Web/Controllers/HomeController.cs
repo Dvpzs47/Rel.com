cdbbeaf2ecd637dbae252751e6ec4e2271fe35d6
Dvpzs 47 <Dvpzs47@gmail.com>
2021-03-02 12:41:27 +0100

    updated packages

--- src/Rel.Web/Controllers/HomeController.cs
+++ src/Rel.Web/Controllers/HomeController.cs
@@ -4,12 +4,11 @@ namespace Rel.Web.Controllers
 {
     public class HomeController : Controller
     {
-        /*
         public IActionResult Index()
         {
             return View();
         }
-        */
+        
         public IActionResult Error()
         {
             return View();
