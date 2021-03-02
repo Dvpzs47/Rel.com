7e13ee1bf88ec145451abc759a6bb1da9b679c99
Dvpzs 47 <Dvpzs47@gmail.com>
2021-03-02 12:36:48 +0100

    create razor home page

--- src/Rel.Web/Controllers/HomeController.cs
+++ src/Rel.Web/Controllers/HomeController.cs
@@ -4,10 +4,12 @@ namespace Rel.Web.Controllers
 {
     public class HomeController : Controller
     {
+        /*
         public IActionResult Index()
         {
             return View();
         }
+        */
         public IActionResult Error()
         {
             return View();
